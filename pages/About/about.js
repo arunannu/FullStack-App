767a70bcbd5d7857e0b677b5ed23479512dafff9
Arun <arunkushwahaayan@gmail.com>
2024-03-06 15:15:40 +0530

    firstCommit

---
+++ pages/About/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <h3>This is frist project using with nextjs...</h3>
+    </div>
+  )
+}
+
+export default about
---
+++ pages/news.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const news = () => {
+  return (
+    <div>
+      <h2>This is news portals..</h2>
+    </div>
+  )
+}
+
+export default news
